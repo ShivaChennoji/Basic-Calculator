83db106cc4d7e90aa07636807971f5e6f6d2db95
shivachennoji <chennojishiva1@gmail.com>
2024-10-01 11:37:34 +0530

    first commit

--- scripts.js
+++ scripts.js
@@ -1,27 +1,45 @@
-let string = "";
-// accessing the buttons
-let buttons = document.querySelectorAll('.button')
-Array.from(buttons).forEach((button) => {
-    button.addEventListener("click", (e) => {
-        if (e.target.innerHTML == "=") {
-            string = eval(string);
-            document.querySelector('input').value = string;
-        } else if (e.target.innerHTML == "C") {
-            string = "";
-            document.querySelector('input').value = string;
-        } else if (e.target.innerHTML == "bac") {
-            string = string.slice(0, -1);
-            document.querySelector('input').value = string;
-        } else if (e.target.innerHTML == "00") {
-            string = string += '00';
-            document.querySelector('input').value = string;
-        } else if (e.target.innerHTML == "000") {
-            string = string += '000';
-            document.querySelector('input').value = string;
-        } else {
-            console.log(e.target)
-            string = string + e.target.innerHTML;
-            document.querySelector('input').value = string;
+let data_input = "";
+const input = document.querySelector('input');
+
+// Accessing the buttons
+document.querySelectorAll('.button').forEach((button) => {
+    button.addEventListener("click", (output) => {
+        const value = output.target.innerHTML;
+        
+        switch (value) {
+            case "=":
+                try {
+                    data_input = eval(data_input);  
+                //Evaluating the string as a math expression
+                    input.value = data_input;
+                } catch (error) {
+                    input.value = "Error";  
+                // Handling invalid input
+                }
+                break;
+            case "C":
+                input = "";
+                input.value = data_input;
+                break;
+            case "bac":
+                data_input = data_input.slice(0, -1);  
+                // Removing the last character
+                input.value = data_input;
+                break;
+            case "00":
+            case "000":
+                input += value;  
+                // Appends multiple zeroes
+                input.value = data_input;
+                break;
+                case "%":
+                data_input=parseFloat((data_input)/100).toString();
+                input.value=data_input;
+                break;
+            default:
+                data_input += value; 
+                 // Appends the clicked button value
+                input.value = data_input;
         }
-    })
+    });
 });
